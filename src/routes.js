0f3b3ac64f6d2b367fb78b6790335895a1234cd6
unknown <marcos_moura@live.com>
2019-07-14 18:05:24 -0300

    Updates + Padrão de Código

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require("express");
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Olá mundo" });
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Olá mundo' });
 });
 
-module.exports = routes;
+export default routes;
