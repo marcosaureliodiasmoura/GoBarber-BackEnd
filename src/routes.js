71a324c1e6fce08995ce920db1252876c4a7ba91
unknown <marcos_moura@live.com>
2019-07-15 10:49:32 -0300

    Cadastrando User

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserControllers';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Marcos Moura',
-    email: 'marcos_moura@live.com',
-    password_hash: '123456',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
