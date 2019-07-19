f191a10c69fee49740553ff597a554e160668f24
marcosaureliodiasmoura <marcos_moura@live.com>
2019-07-19 19:28:15 -0300

    Listando Agendamento dos Providers

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -5,10 +5,15 @@ import User from '../models/User';
 import File from '../models/File';
 
 class AppointmentController {
+  // Listagem pro usuário comum
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       attributes: ['id', 'date'],
       include: [
         {
